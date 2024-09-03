9c516f3ce1ac28302d3a5e6a2b583cdeafc4460b
yusufriduan <muhammad.yusuf.riduan@student.mmu.edu.my>
2024-09-03 21:56:57 +0800

    Delete the test from the main branch

--- test.py
+++
@@ -1,8 +0,0 @@
-leaderboard_file = "leaderboard.txt"
-def read_leaderboard():
-    with open(leaderboard_file, 'r') as f:
-        return f.read()
-
-leaderboard_contents = read_leaderboard()
-print(leaderboard_contents)
-q = input("Press enter to continue.")
